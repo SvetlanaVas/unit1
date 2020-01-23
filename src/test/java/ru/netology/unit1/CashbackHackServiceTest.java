6fb6c332685107b159e3aca2187389b7f4f918f7
Svetlana Vasilevskaya <svetavas@bk.ru>
2020-01-23 13:35:25 +0300

    ParameterizedTest

--- src/test/java/ru/netology/unit1/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit1/CashbackHackServiceTest.java
@@ -1,34 +1,38 @@
 package ru.netology.unit1;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
 
-    @Test
-    void shouldReturn100IfAmountIs900() {
+    /*    @Test
+         void shouldReturn100IfAmountIs900() {
 
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 900;
+           CashbackHackService cashbackHackService = new CashbackHackService();
+            int amount = 900;
 
-        int actual = cashbackHackService.remain(amount);
-        int expected = 100;
+          int actual = cashbackHackService.remain(amount);
+            int expected = 100;
 
-        assertEquals(expected, actual);
-    }
+            assertEquals(expected, actual);
+        }
 
-    @Test
-    void shouldReturn0IfAmountIs1000() {
+        @Test
+        void shouldReturn0IfAmountIs1000() {
 
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1000;
+            CashbackHackService cashbackHackService = new CashbackHackService();
+            int amount = 1000;
 
-        int actual = cashbackHackService.remain(amount);
-        int expected = 0;
+            int actual = cashbackHackService.remain(amount);
+            int expected = 0;
 
-        assertEquals(expected, actual);
-    }
+            assertEquals(expected, actual);
+        }
+
+     */
     @Test
     void shouldExceptionIfAmountIsNegativ() {
 
@@ -38,4 +42,13 @@ class CashbackHackServiceTest {
         assertThrows(IllegalArgumentException.class, () -> cashbackHackService.remain(amount));
     }
 
+    @ParameterizedTest
+    @CsvFileSource(resources = "/Data.csv", numLinesToSkip = 1)
+    void shouldCashback(int amount, int expected) {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(expected, actual);
+    }
 }
